955f02588714bf1e8af5dce09006987f79d6097a
Misbah <misbah.uddin.sagor@gmail.com>
2022-04-27 06:12:42 +0600

    first commit

---
+++ js/utils.js
@@ -0,0 +1,24 @@
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
+
+//determine winner
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('.prompt').style.display = 'flex'
+
+    if (player.health === enemy.health) {
+        document.querySelector('.prompt').innerHTML = 'Tie'
+    }
+    else if (player.health > enemy.health) {
+        document.querySelector('.prompt').innerHTML = 'Player 1 Wins'
+    }
+    else if (player.health < enemy.health) {
+        document.querySelector('.prompt').innerHTML = 'Player 2 Wins'
+    }
+}
